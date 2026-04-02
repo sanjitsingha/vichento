8aad5da00946b8973f74d2a7a5cf3a62a7ba64cc
Sanjit Singha <111037110+sanjitsingha@users.noreply.github.com>
2026-04-02 06:55:20 +0000

    privacy-policy-page

--- src/app/privacy-policy/page.jsx
+++ src/app/privacy-policy/page.jsx
@@ -3,11 +3,211 @@ import React from 'react'
 const page = () => {
   return (
     <div className='w-full'>
-      <div className='max-w-[800px] w-full mx-auto pt-10'>
-        <h1 className='text-2xl font-bold mb-4 font-creato  '>Privacy Policy</h1>
-        <p className='text-gray-600 '>
-          This is a simple privacy policy page. You can add your own content here.
-        </p>
+      <div className='max-w-[800px] w-full mx-auto md:pt-10 p-4'>
+        <h1 className='text-2xl font-creato text-black '>Privacy Policy</h1>
+        <p className='text-xs text-gray-500 mt-2'>Effective Date: March 23, 2026</p>
+        <section>
+          <p className='text-gray-600 text-sm mt-4'>
+            Welcome to Vichento (“we,” “our,” or “us”). Your privacy is important to us. This Privacy Policy explains how we collect, use, disclose, and safeguard your information when you visit www.vichento.com.
+
+            By using our platform, you agree to the terms of this Privacy Policy.
+          </p>
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>1. Information We Collect</h2>
+            <p className='text-sm font-semibold mt-2'>a. Personal Information</p>
+            <p className='text-sm text-gray-500 mt-2'>
+              When you register or use Vichento, we may collect:
+
+              <ul className='list-disc list-inside mt-2 '>
+                <li>Name</li>
+                <li>Email address</li>
+                <li>Profile image</li>
+                <li>User preferences (e.g., sports, entertainment)</li>
+                <li>Any additional profile details you provide</li>
+              </ul>
+            </p>
+
+            <p className='text-sm font-semibold mt-2'>b. User-Generated Content</p>
+            <p className='text-sm text-gray-500 mt-2'>
+              Articles, blog posts, comments, likes, and saved content created or interacted with on the platform
+            </p>
+            <p className='text-sm font-semibold mt-2'>c. Authentication Data</p>
+            <p className='text-sm text-gray-500 mt-2'>
+              We use third-party authentication services such as Appwrite and Firebase Authentication, which may collect and process login credentials securely.
+            </p>
+            <p className='text-sm font-semibold mt-2'>d. Usage Data</p>
+            <p className='text-sm text-gray-500 mt-2'>
+              We automatically collect certain information, including:
+
+              <ul className='list-disc list-inside mt-2 '>
+                <li>IP address</li>
+                <li>Browser type and version</li>
+                <li>Device information</li>
+                <li>Pages visited and time spent</li>
+                <li>Interaction behavior on the platform</li>
+              </ul>
+            </p>
+          </div>
+
+
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>2. How We Use Your Information</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We use your information to:
+            </p>
+            <ul className='list-disc list-inside mt-2 text-sm text-gray-500 '>
+              <li>Create and manage your account</li>
+              <li>Personalize your experience based on preferences</li>
+              <li>Enable publishing, commenting, and engagement features</li>
+              <li>Improve platform performance and user experience</li>
+              <li>Monitor usage and detect fraud or misuse</li>
+              <li>Communicate updates, notifications, and important information</li>
+              <li>Support monetization features such as affiliate links and paid services</li>
+            </ul>
+          </div>
+
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>3. Cookies and Tracking Technologies </h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We use cookies and similar technologies to:
+            </p>
+            <ul className='list-disc list-inside mt-2 text-sm text-gray-500 '>
+              <li>Maintain login sessions (via Appwrite and Firebase Authentication)</li>
+              <li>Analyze traffic and user behavior (Google Analytics, Umami)</li>
+              <li>Store user preferences for a better experience</li>
+
+            </ul>
+            <p className='text-sm mt-2 text-gray-500'>You can control or disable cookies through your browser settings.</p>
+          </div>
+
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>4. Third-Party Services</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We may use third-party services that collect, process, and store your data:
+            </p>
+            <ul className='list-disc list-inside mt-2 text-sm text-gray-500 '>
+              <li>Appwrite – Backend, database, and authentication</li>
+              <li>Firebase Authentication – Secure login and identity management</li>
+              <li>Google Analytics – Website analytics and traffic insights</li>
+              <li>Umami – Privacy-focused analytics</li>
+
+            </ul>
+            <p className='text-sm mt-2 text-gray-500'>These services have their own privacy policies and we encourage you to review them.</p>
+          </div>
+
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>5. Sharing of Information</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We do not sell your personal data. However, we may share information:
+            </p>
+            <ul className='list-disc list-inside mt-2 text-sm text-gray-500 '>
+              <li>With service providers to operate the platform</li>
+              <li>When required by law or legal processes</li>
+              <li>To protect our rights, users, or platform security</li>
+
+            </ul>
+
+          </div>
+
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>6. Affiliate Links and Monetization</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              Vichento may include affiliate links. If you click on these links, third-party websites may collect data according to their own privacy policies.
+            </p>
+
+            <p className='text-sm mt-2 text-gray-500'>We may also introduce paid features or services, and relevant data may be processed to enable these transactions.</p>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>7. Data Security</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We implement reasonable security measures to protect your data. However, no method of transmission over the internet is 100% secure.
+            </p>
+          </div>
+        </section>
+         <section>
+          <div>
+            <h2 className='font-bold mt-3'>8. Data Retention</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We retain your data:
+            </p>
+            <ul className='list-disc list-inside mt-2 text-sm text-gray-500 '>
+              <li>As long as your account is active</li>
+              <li>As necessary to comply with legal obligations</li>
+              <li>To resolve disputes and enforce agreements</li>
+
+            </ul>
+            <p className='text-sm mt-2 text-gray-500'>You may request deletion of your account and associated data at any time.</p>
+          </div>
+
+        </section>
+        <section>
+          <div>
+            <h2 className='font-bold mt-3'>9. Your Rights</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              Depending on your location, you may have the right to:
+            </p>
+            <ul className='list-disc list-inside mt-2 text-sm text-gray-500 '>
+              <li>Access your personal data</li>
+              <li>Correct or update your information</li>
+              <li>Request deletion of your data</li>
+              <li>Withdraw consent</li>
+
+            </ul>
+            <p className='text-sm mt-2 text-gray-500'>To exercise these rights, contact us at: <span className='text-black font-semibold'>
+                contact@vichento.com
+              </span></p>
+          </div>
+
+        </section>
+         <section>
+          <div>
+            <h2 className='font-bold mt-3'>10. Children’s Privacy</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              Vichento is not intended for children under the age of 13. We do not knowingly collect data from children.
+            </p>
+           
+          </div>
+
+        </section>
+           <section>
+          <div>
+            <h2 className='font-bold mt-3'>11. Changes to This Privacy Policy</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              We may update this Privacy Policy from time to time. Changes will be posted on this page with an updated effective date.
+            </p>
+           
+          </div>
+
+        </section>
+          <section>
+          <div>
+            <h2 className='font-bold mt-3'>12. Contact Us</h2>
+            <p className='text-sm mt-2 text-gray-500'>
+              If you have any questions or concerns about this Privacy Policy, please contact us at: <span className='text-black font-semibold'>
+                contact@vichento.com
+              </span>
+            </p>
+            <hr className='my-10 opacity-20' />
+            <p className='text-sm text-black mb-5'>By using Vichento, you acknowledge that you have read and understood this Privacy Policy.</p>
+           
+          </div>
+
+        </section>
+        
       </div>
     </div>
   )
