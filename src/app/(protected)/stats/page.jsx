d09ddf2d69cfb2fdad3ca1cfd15a388e4f593e03
sanjitsingha <itsmesanjitsingh@gmail.com>
2026-04-22 12:30:07 +0530

    stats page

--- src/app/(protected)/stats/page.jsx
+++ src/app/(protected)/stats/page.jsx
@@ -1,6 +1,9 @@
 import { ArrowTrendingUpIcon } from '@heroicons/react/24/outline'
 import React from 'react'
+import Image from 'next/image'
 
+import { RxShare2 } from "react-icons/rx";
+import { BsThreeDots } from "react-icons/bs";
 
 const page = () => {
   return (
@@ -11,25 +14,102 @@ const page = () => {
           <ArrowTrendingUpIcon className="text-gray-500 size-6" />
         </div>
         <div className='w-full grid grid-cols-3 gap-4  h-30 '>
-          <div className='bg-white p-4 border border-gray-300 rounded'>
+          <div className='bg-white p-4 border border-gray-300 rounded transition-all ease-in-out duration-200 hover:shadow-xl'>
             <p className='text-gray-500 text-xs'>Total Post</p>
             <p className='text-black text-3xl mt-2 font-creato font-bold'>100</p>
           </div>
-          <div className='bg-white p-4 border border-gray-300 rounded'>
+          <div className='bg-white p-4 border border-gray-300 rounded transition-all ease-in-out duration-200 hover:shadow-xl'>
             <p className='text-gray-500 text-xs'>Total Views</p>
             <p className='text-black text-3xl mt-2 font-creato font-bold'>100</p>
           </div>
-          <div className='bg-white p-4 border border-gray-300 rounded'>
+          <div className='bg-white p-4 border border-gray-300 rounded transition-all ease-in-out duration-200 hover:shadow-xl'>
             <p className='text-gray-500 text-xs'>Total Likes</p>
             <p className='text-black text-3xl mt-2 font-creato font-bold'>100</p>
           </div>
 
         </div>
         <div className='w-full  h-100 grid grid-cols-2 gap-4 mt-10'>
-          <div className='w-full border border-gray-300 rounded '>
+          <div className='w-full p-4 border border-gray-300 rounded '>
+            <p className='text-black text-xl font-creato font-bold'>Latest Published</p>
+            <div className='w-full   h-full mt-4 '>
+              <div className='w-full gap-3 h-fit border-b pb-4 border-gray-300  flex  rounded mt-2'>
+                <div className=' overflow-hidden'>
+                  <Image src={'/placeholder.png'} width={100} height={100} alt='title' className='rounded-sm' />
+                </div>
+                <div className='flex flex-col justify-center'>
+                  <p className=' text-[16px]  line-clamp-1  text-gray-800 font-creato'>this will be the article title</p>
+                  <p className='text-xs text-gray-400'>22-04-2026</p>
+                </div>
+              </div>
+              <div className='w-full h-fit pt-4 '>
+                <div className='w-full flex justify-between'>
+                  <p className='text-gray-500 text-sm'>Total Views:</p>
+                  <p className='text-black text-lg font-creato font-bold'>50</p>
+                </div>
+                <div className='w-full flex mt-4 justify-between'>
+                  <p className='text-gray-500 text-sm'>Total Like:</p>
+                  <p className='text-black text-lg font-creato font-bold'>50</p>
+                </div>
+                <div className="flex items-center justify-between gap-2 mt-4">
+                  <button className='bg-gray-100 border-gray-300 border transition-all ease-in-out duration-200 w-fit mt-4 cursor-pointer active:scale-90 flex items-center gap-2 text-black text-sm px-6 rounded py-2'>
+                    <p className='text-gray-500 text-sm font-creato'>Share</p>  <RxShare2 className='text-gray-500' size={20} />
+                  </button>
+                  <button className=' justify-center items-center transition-all ease-in-out duration-200 w-12 h-12 mt-4 cursor-pointer active:scale-80 flex  gap-2 text-black text-sm  rounded py-2'>
+                    <BsThreeDots size={22} />
+                  </button>
+                </div>
+
+
+              </div>
+
+            </div>
 
           </div>
-          <div className='w-full  border border-gray-300 rounded'>
+          <div className='w-full  border border-gray-300 p-4 rounded'>
+            <p className='text-black text-xl font-creato font-bold'>Drafts</p>
+            <div className='w-full  h-full '>
+              <div className='w-full gap-2 h-fit pb-4 border-b  flex items-center border-gray-300 rounded mt-2'>
+                <div className='h-full w-20 overflow-hidden'>
+                  <Image src={'/placeholder.png'} width={80} height={80} alt='title' className='rounded-sm' />
+                </div>
+                <div className='flex flex-col justify-center'>
+                  <p className='text-sm text-gray-800 font-creato'>this will be the article title</p>
+                  <p className='text-xs text-gray-400'>22-04-2026</p>
+                </div>
+                <div className='ml-auto bg-black/10 py-[2px] px-2 rounded-sm h-fit'>
+                  <p className='text-xs text-gray-400'>Draft</p>
+
+                </div>
+              </div>
+              <div className='w-full gap-2 h-fit pb-4 border-b  flex items-center border-gray-300 rounded mt-2'>
+                <div className='h-full w-20 overflow-hidden'>
+                  <Image src={'/placeholder.png'} width={80} height={80} alt='title' className='rounded-sm' />
+                </div>
+                <div className='flex flex-col justify-center'>
+                  <p className='text-sm text-gray-800 font-creato'>this will be the article title</p>
+                  <p className='text-xs text-gray-400'>22-04-2026</p>
+                </div>
+                <div className='ml-auto bg-black/10 py-[2px] px-2 rounded-sm h-fit'>
+                  <p className='text-xs text-gray-400'>Draft</p>
+
+                </div>
+              </div>
+              <div className='w-full gap-2 h-fit pb-4 border-b  flex items-center border-gray-300 rounded mt-2'>
+                <div className='h-full w-20 overflow-hidden'>
+                  <Image src={'/placeholder.png'} width={80} height={80} alt='title' className='rounded-sm' />
+                </div>
+                <div className='flex flex-col justify-center'>
+                  <p className='text-sm text-gray-800 font-creato'>this will be the article title</p>
+                  <p className='text-xs text-gray-400'>22-04-2026</p>
+                </div>
+                <div className='ml-auto bg-black/10 py-[2px] px-2 rounded-sm h-fit'>
+                  <p className='text-xs text-gray-400'>Draft</p>
+
+                </div>
+              </div>
+
+            </div>
+
 
           </div>
 
